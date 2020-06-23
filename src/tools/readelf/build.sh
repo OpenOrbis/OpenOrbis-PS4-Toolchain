7af9a30d5849ba2152da69d442e618eaf633244c
Specter <cryptogenicdev@gmail.com>
2020-06-23 16:22:40 -0400

    Fix up create-eboot and readelf scripts + Fixed create-eboot and readelf scripts to also cross-compile to macOS + Added batch build script for readelf

--- src/tools/readelf/build.sh
+++ src/tools/readelf/build.sh
@@ -1,16 +1,11 @@
 ## Windows
-# create-eboot
 GOOS=windows go build -o readelf.exe
 mv ./readelf.exe ${OO_PS4_TOOLCHAIN}/bin/windows/readelf.exe
 
-## Linux and macOS
-
-if [ "$uname_s" = "Linux" ] ; then
-  cdir="linux"
-elif [ "$uname_s" = "Darwin" ] ; then
-  cdir="macos"
-fi
-
-# create-eboot
+## Linux
 go build -o readelf
-mv ./readelf ${OO_PS4_TOOLCHAIN}/bin/${cdir}/readelf
+mv ./readelf ${OO_PS4_TOOLCHAIN}/bin/linux/readelf
+
+## MacOS
+GOOS=darwin GOARCH=amd64 go build -o readelfosx
+mv ./readelfosx ${OO_PS4_TOOLCHAIN}/bin/macos/readelf
