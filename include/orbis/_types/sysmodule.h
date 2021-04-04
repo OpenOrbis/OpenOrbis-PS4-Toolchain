96280097377e9ac427145754364526e0073453d7
0x199 <e1e@live.nl>
2021-04-05 01:24:10 +0200

    Update sysmodule.h

--- include/orbis/_types/sysmodule.h
+++ include/orbis/_types/sysmodule.h
@@ -67,53 +67,53 @@
 #define ORBIS_SYSMODULE_REMOTE_PLAY 0x00B6 // libSceRemoteplay.sprx
 #define ORBIS_SYSMODULE_USBD 0x00B7 // libSceUsbd.sprx
 #define ORBIS_SYSMODULE_GAME_CUSTOM_DATA_DIALOG 0x00B8 // libSceGameCustomDataDialog.sprx
-#define ORBIS_SYSMODULE_RESERVED0 0x00B9 // libSceNpEulaDialog.sprx
-#define ORBIS_SYSMODULE_RESERVED1 0x00BA // libSceRandom.sprx
+#define ORBIS_SYSMODULE_NP_EULA_DIALOG 0x00B9 // libSceNpEulaDialog.sprx
+#define ORBIS_SYSMODULE_RANDOM 0x00BA // libSceRandom.sprx
 #define ORBIS_SYSMODULE_RESERVED2 0x00BB
 #define ORBIS_SYSMODULE_M4AAC_ENC 0x00BC // libSceM4aacEnc.sprx
 #define ORBIS_SYSMODULE_AUDIODEC_CPU 0x00BD // libSceAudiodecCpu.sprx
 #define ORBIS_SYSMODULE_AUDIODEC_CPU_DDP 0x00BE // libSceAudiodecCpuDdp.sprx
 #define ORBIS_SYSMODULE_AUDIODEC_CPU_M4AAC 0x00C0 // libSceAudiodecCpuM4aac.sprx
-#define ORBIS_SYSMODULE_RESERVED3 0x00C1 // libSceBemp2sys.sprx
-#define ORBIS_SYSMODULE_RESERVED4 0x00C2 // libSceBeisobmf.sprx
-#define ORBIS_SYSMODULE_RESERVED5 0x00C3 // libScePlayReady.sprx
-#define ORBIS_SYSMODULE_RESERVED6 0x00C4 // libSceVideoNativeExtEssential.sprx
+#define ORBIS_SYSMODULE_BEMP2_SYS 0x00C1 // libSceBemp2sys.sprx
+#define ORBIS_SYSMODULE_BEISOBMF 0x00C2 // libSceBeisobmf.sprx
+#define ORBIS_SYSMODULE_PLAY_READY 0x00C3 // libScePlayReady.sprx
+#define ORBIS_SYSMODULE_VIDEO_NATIVE_EXT_ESSENTIAL 0x00C4 // libSceVideoNativeExtEssential.sprx
 #define ORBIS_SYSMODULE_ZLIB 0x00C5 // libSceZlib.sprx
-#define ORBIS_SYSMODULE_RESERVED8 0x00C6 // libSceDtcpIp.sprx
+#define ORBIS_SYSMODULE_DTCP_IP 0x00C6 // libSceDtcpIp.sprx
 #define ORBIS_SYSMODULE_CONTENT_SEARCH 0x00C7 // libSceContentSearch.sprx
-#define ORBIS_SYSMODULE_RESERVED9 0x00C8 // libSceShareUtility.sprx
+#define ORBIS_SYSMODULE_SHARE_UTILITY 0x00C8 // libSceShareUtility.sprx
 #define ORBIS_SYSMODULE_AUDIODEC_CPU_DTS_HD_LBR 0x00C9 // libSceAudiodecCpuDtsHdLbr.sprx
 #define ORBIS_SYSMODULE_DECI4H 0x00CA
 #define ORBIS_SYSMODULE_HEAD_TRACKER 0x00CB
-#define ORBIS_SYSMODULE_RESERVED11 0x00CC // libSceGameUpdate.sprx
-#define ORBIS_SYSMODULE_RESERVED12 0x00CD // libSceAutoMounterClient.sprx
+#define ORBIS_SYSMODULE_GAME_UPDATE 0x00CC // libSceGameUpdate.sprx
+#define ORBIS_SYSMODULE_AUTO_MOUNTER_CLIENT 0x00CD // libSceAutoMounterClient.sprx
 #define ORBIS_SYSMODULE_SYSTEM_GESTURE 0x00CE // libSceSystemGesture.sprx
 #define ORBIS_SYSMODULE_VIDEODEC2 0x00CF
-#define ORBIS_SYSMODULE_RESERVED14 0x00D0 // libSceVdecwrap.sprx
+#define ORBIS_SYSMODULE_VDECWRAP 0x00D0 // libSceVdecwrap.sprx
 #define ORBIS_SYSMODULE_AT9_ENC 0x00D1 // libSceAt9Enc.sprx
 #define ORBIS_SYSMODULE_CONVERT_KEYCODE 0x00D2 // libSceConvertKeycode.sprx
 #define ORBIS_SYSMODULE_SHARE_PLAY 0x00D3 // libSceSharePlay.sprx
 #define ORBIS_SYSMODULE_HMD 0x00D4 // libSceHmd.sprx
-#define ORBIS_SYSMODULE_RESERVED18 0x00D5 // libSceUsbStorage.sprx
-#define ORBIS_SYSMODULE_RESERVED16 0x00D6 // libSceUsbStorageDialog.sprx
-#define ORBIS_SYSMODULE_RESERVED17 0x00D7 // libSceDiscMap.sprx
+#define ORBIS_SYSMODULE_USB_STORAGE 0x00D5 // libSceUsbStorage.sprx
+#define ORBIS_SYSMODULE_USB_STORAGE_DIALOG 0x00D6 // libSceUsbStorageDialog.sprx
+#define ORBIS_SYSMODULE_DISC_MAP 0x00D7 // libSceDiscMap.sprx
 #define ORBIS_SYSMODULE_FACE_TRACKER 0x00D8
 #define ORBIS_SYSMODULE_HAND_TRACKER 0x00D9
-#define ORBIS_SYSMODULE_RESERVED19 0x00DA // libSceNpSnsYouTubeDialog.sprx
-#define ORBIS_SYSMODULE_RESERVED20 0x00DC // libSceProfileCacheExternal.sprx
-#define ORBIS_SYSMODULE_RESERVED21 0x00DD // libSceMusicPlayerService.sprx
-#define ORBIS_SYSMODULE_RESERVED22 0x00DE // libSceSpSysCallWrapper.sprx
-#define ORBIS_SYSMODULE_RESERVED23 0x00DF // libScePs2EmuMenuDialog.sprx
-#define ORBIS_SYSMODULE_RESERVED24 0x00E0 // libSceNpSnsDailyMotionDialog.sprx
+#define ORBIS_SYSMODULE_NP_SNS_YOUTUBE_DIALOG 0x00DA // libSceNpSnsYouTubeDialog.sprx
+#define ORBIS_SYSMODULE_PROFILE_CACHE_EXTERNAL 0x00DC // libSceProfileCacheExternal.sprx
+#define ORBIS_SYSMODULE_MUSIC_PLAYER_SERVICE 0x00DD // libSceMusicPlayerService.sprx
+#define ORBIS_SYSMODULE_SP_SYS_CALL_WRAPPER 0x00DE // libSceSpSysCallWrapper.sprx
+#define ORBIS_SYSMODULE_PS2_EMU_MENU_DIALOG 0x00DF // libScePs2EmuMenuDialog.sprx
+#define ORBIS_SYSMODULE_NP_SNS_DIALYMOTION_DIALOG 0x00E0 // libSceNpSnsDailyMotionDialog.sprx
 #define ORBIS_SYSMODULE_AUDIODEC_CPU_HEVAG 0x00E1 // libSceAudiodecCpuHevag.sprx
 #define ORBIS_SYSMODULE_LOGIN_DIALOG 0x00E2 // libSceLoginDialog.sprx
 #define ORBIS_SYSMODULE_LOGIN_SERVICE 0x00E3 // libSceLoginService.sprx
 #define ORBIS_SYSMODULE_SIGNIN_DIALOG 0x00E4 // libSceSigninDialog.sprx
-#define ORBIS_SYSMODULE_RESERVED35 0x00E5 // libSceVdecsw.sprx
-#define ORBIS_SYSMODULE_RESERVED25 0x00E6 // libSceCustomMusicCore.sprx
+#define ORBIS_SYSMODULE_VDECSW 0x00E5 // libSceVdecsw.sprx
+#define ORBIS_SYSMODULE_CUSTOM_MUSIC_CORE 0x00E6 // libSceCustomMusicCore.sprx
 #define ORBIS_SYSMODULE_JSON2 0x00E7 // libSceJson2.sprx
 #define ORBIS_SYSMODULE_AUDIO_LATENCY_ESTIMATION 0x00E8
-#define ORBIS_SYSMODULE_RESERVED26 0x00E9 // libSceWkFontConfig.sprx
+#define ORBIS_SYSMODULE_WK_FONT_CONFIG 0x00E9 // libSceWkFontConfig.sprx
 #define ORBIS_SYSMODULE_RESERVED27 0x00EA
 #define ORBIS_SYSMODULE_HMD_SETUP_DIALOG 0x00EB // libSceHmdSetupDialog.sprx
 #define ORBIS_SYSMODULE_RESERVED28 0x00EC
@@ -124,14 +124,14 @@
 #define ORBIS_SYSMODULE_PLAYGO_DIALOG 0x00F1 // libScePlayGoDialog.sprx
 #define ORBIS_SYSMODULE_SOCIAL_SCREEN 0x00F2 // libSceSocialScreen.sprx
 #define ORBIS_SYSMODULE_EDIT_MP4 0x00F3 // libSceEditMp4.sprx
-#define ORBIS_SYSMODULE_RESERVED37 0x00F5 // libScePsmKitSystem.sprx
+#define ORBIS_SYSMODULE_PSM_KIT_SYSTEM 0x00F5 // libScePsmKitSystem.sprx
 #define ORBIS_SYSMODULE_TEXT_TO_SPEECH 0x00F6 // libSceTextToSpeech.sprx
 #define ORBIS_SYSMODULE_NP_TOOLKIT 0x00F7
-#define ORBIS_SYSMODULE_RESERVED38 0x00F8 // libSceCustomMusicService.sprx
-#define ORBIS_SYSMODULE_RESERVED39 0x00F9 // libSceClSysCallWrapper.sprx
-#define ORBIS_SYSMODULE_RESERVED40 0x00FA // libSceSystemLogger.sprx
+#define ORBIS_SYSMODULE_CUSTOM_MUSIC_SERVICE 0x00F8 // libSceCustomMusicService.sprx
+#define ORBIS_SYSMODULE_CL_SYS_CALL_WRAPPER 0x00F9 // libSceClSysCallWrapper.sprx
+#define ORBIS_SYSMODULE_SYSTEM_LOGGER 0x00FA // libSceSystemLogger.sprx
 #define ORBIS_SYSMODULE_BLUETOOTH_HID 0x00FB // libSceBluetoothHid.sprx
-#define ORBIS_SYSMODULE_RESERVED41 0x00FC // libSceVideoDecoderArbitration.sprx
+#define ORBIS_SYSMODULE_VIDEO_DECODER_ARBITRATION 0x00FC // libSceVideoDecoderArbitration.sprx
 #define ORBIS_SYSMODULE_VR_SERVICE_DIALOG 0x00FD // libSceVrServiceDialog.sprx
 #define ORBIS_SYSMODULE_JOB_MANAGER 0x00FE
 
