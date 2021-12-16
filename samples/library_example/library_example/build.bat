b02f636fda9948a901a9495a5cfebafb08311d64
SpecterDev <cryptogenicdev@gmail.com>
2021-12-16 00:59:54 -0500

    Update library sample build scripts

--- samples/library_example/library_example/build.bat
+++ samples/library_example/library_example/build.bat
@@ -3,6 +3,8 @@ SETLOCAL EnableDelayedExpansion
 Rem Libraries to link in
 set libraries=-lSceLibcInternal -lkernel
 
+Rem set extra_flags=
+
 Rem Read the script arguments into local vars
 set intdir=%1
 set targetname=%~2
@@ -17,7 +19,7 @@ set outputStub=%intdir%\%targetname%_stub.so
 
 Rem Compile object files for all the source files
 for %%f in (*.cpp) do (
-    clang++ -cc1 -triple x86_64-scei-ps4-elf -munwind-tables -I"%OO_PS4_TOOLCHAIN%\include" -emit-obj -o %intdir%\%%~nf.o %%~nf.cpp
+    clang++ --target=x86_64-pc-freebsd12-elf -fPIC -funwind-tables -c -I"%OO_PS4_TOOLCHAIN%\include" %extra_flags% -emit-obj -o %intdir%\%%~nf.o %%~nf.cpp
 )
 
 Rem Get a list of object files for linking
@@ -25,7 +27,7 @@ set obj_files=
 for %%f in (%intdir%\\*.o) do set obj_files=!obj_files! .\%%f
 
 Rem Link the input ELF
-ld.lld -m elf_x86_64 -pie --script "%OO_PS4_TOOLCHAIN%\link.x" --eh-frame-hdr -o "%outputElf%" "-L%OO_PS4_TOOLCHAIN%\lib" %libraries% --verbose "%OO_PS4_TOOLCHAIN%\lib\crtlib.o" %obj_files%
+ld.lld -m elf_x86_64 -pie --script "%OO_PS4_TOOLCHAIN%\link.x" --eh-frame-hdr -o "%outputElf%" "-L%OO_PS4_TOOLCHAIN%\lib" -lSceLibcInternal -lkernel --verbose "%OO_PS4_TOOLCHAIN%\lib\crtlib.o" %obj_files%
 
 Rem Create stub shared libraries
 for %%f in (*.cpp) do (
@@ -38,7 +40,7 @@ for %%f in (%intdir%\\*.o.stub) do set stub_obj_files=!stub_obj_files! .\%%f
 clang++ -target x86_64-pc-linux-gnu -shared -fuse-ld=lld -ffreestanding -nostdlib -fno-builtin "-L%OO_PS4_TOOLCHAIN%\lib" %libraries% %stub_obj_files% -o "%outputStub%"
 
 Rem Create the prx
-%OO_PS4_TOOLCHAIN%\bin\windows\create-lib.exe -in "%outputElf%" --out "%outputOelf%"
+%OO_PS4_TOOLCHAIN%\bin\windows\create-fself.exe -in "%outputElf%" --out "%outputOelf%" --lib "%outputPrx%"
 
 Rem Cleanup
 copy "%outputPrx%" "%outputPath%\%targetname%.prx"
