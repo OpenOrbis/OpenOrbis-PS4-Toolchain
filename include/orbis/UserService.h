0df86e5dcd54712784da5162944e4ee18ea838a5
xXxTheDarkprogramerxXx <eonvwyk@gmail.com>
2022-01-07 06:58:17 +0200

    Update

--- include/orbis/UserService.h
+++ include/orbis/UserService.h
@@ -388,16 +388,3 @@ void sceUserServiceUnregisterEventCallback();
 }
 #endif
 #endif
-
-© 2022 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
