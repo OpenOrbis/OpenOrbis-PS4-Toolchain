a9a1dbd482e954526df979323e95539ac1dafabb
Backporter <29647778+Backporter@users.noreply.github.com>
2022-03-19 03:12:55 -0400

    Add support for error dialog
    Adds structure and function definitions.

--- include/orbis/ErrorDialog.h
+++ include/orbis/ErrorDialog.h
@@ -1,32 +1,39 @@
 #ifndef _SCE_ERROR_DIALOG_H_
 #define _SCE_ERROR_DIALOG_H_
 
+#include "_types/error_dialog.h"
 #include <stdint.h>
 
 #ifdef __cplusplus 
 extern "C" {
 #endif
 
-
-// Empty Comment
+// closes the opened error dialog
 void sceErrorDialogClose();
-// Empty Comment
-void sceErrorDialogGetStatus();
-// Empty Comment
-void sceErrorDialogInitialize();
-// Empty Comment
-void sceErrorDialogOpen();
-// Empty Comment
-void sceErrorDialogOpenDetail();
-// Empty Comment
-void sceErrorDialogOpenWithReport();
-// Empty Comment
-void sceErrorDialogTerminate();
-// Empty Comment
-void sceErrorDialogUpdateStatus();
 
-#endif
+// returns the status of the running error dialog 
+OrbisErrorDialogStatus sceErrorDialogGetStatus();
+
+// Initializes the error dialog libary, this needs to be called prior to using this library
+int32_t sceErrorDialogInitialize();
+
+// Opens the error dialog with the set Params
+int32_t sceErrorDialogOpen(OrbisErrorDialogParam*);
+
+// Unkown, first arg might be OrbisErrorDialogParam?
+void sceErrorDialogOpenDetail(void*, char*, char*, char*, char*);
+
+// Unkown, first arg might be OrbisErrorDialogParam?
+void sceErrorDialogOpenWithReport(void*, char*);
+
+// force clsoing of the error dialog
+void sceErrorDialogTerminate();
+
+// Gets the updated status of the currently running error dialog
+OrbisErrorDialogStatus sceErrorDialogUpdateStatus();
+
 
 #ifdef __cplusplus
 }
+#endif
 #endif
