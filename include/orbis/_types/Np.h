00e7e5efc1d88e7a25dc12b02d39afc669f048f0
Backporter <29647778+Backporter@users.noreply.github.com>
2022-10-14 20:10:39 -0400

    Update Np.h
    add comments back.

--- include/orbis/_types/Np.h
+++ include/orbis/_types/Np.h
@@ -4,7 +4,7 @@
 /* Np Trophy */
 typedef struct OrbisNpTrophyGameDetails 
 {
-	size_t size;
+	size_t size;			// This needs to be set with sizeof(OrbisNpTrophyGameDetails) or any function that takes this structure as a parameter will return a error(0x80551604)
 	uint32_t numGroups;
 	uint32_t numTrophies;
 	uint32_t numPlatinum;
@@ -17,7 +17,7 @@ typedef struct OrbisNpTrophyGameDetails
 
 typedef struct OrbisNpTrophyGameData 
 {
-	size_t size;
+	size_t size;			// This needs to be set with sizeof(OrbisNpTrophyGameData) or any function that takes this structure as a parameter will return a error(0x80551604)
 	uint32_t unlockedTrophies;
 	uint32_t unlockedPlatinum;
 	uint32_t unlockedGold;
@@ -28,7 +28,7 @@ typedef struct OrbisNpTrophyGameData
 
 typedef struct OrbisNpTrophyGroupDetails 
 {
-	size_t size;
+	size_t size;			// This needs to be set with sizeof(OrbisNpTrophyGroupDetails) or any function that takes this structure as a parameter will return a error(0x80551604)
 	int32_t groupId;
 	uint32_t numTrophies;
 	uint32_t numPlatinum;
@@ -41,7 +41,7 @@ typedef struct OrbisNpTrophyGroupDetails
 
 typedef struct OrbisNpTrophyGroupData
 {
-	size_t size;
+	size_t size;				// This needs to be set with sizeof(OrbisNpTrophyGroupData) or any function that takes this structure as a parameter will return a error(0x80551604)
 	int32_t groupId;
 	uint32_t unlockedTrophies;
 	uint32_t unlockedPlatinum;
@@ -55,39 +55,39 @@ typedef struct OrbisNpTrophyGroupData
 // 
 typedef struct OrbisNpTrophyDetails
 {
-	size_t size;
-	int32_t trophyId;
-	int32_t trophyGrade;
-	int32_t groupId;
-	bool hidden;
-	char Unk02[3];
-	char name[128];
-	char description[1024];
+	size_t size; 			// This needs to be set with sizeof(OrbisNpTrophyDetails) or any function that takes this structure as a parameter will return a error(0x80551604)
+	int32_t trophyId; 		// Trophy ID, 1-XXXX(0 should be the Platinum Trophy)
+	int32_t trophyGrade;	// gold/silver/bronze/Platinum
+	int32_t groupId;		// group where the trophy is located(normally 0 for single group trophy packs, depending on how many trophies exists and are split into groups inside the pack this could rage from 0 - xxxx)
+	bool hidden; 			// this determines if you can see the trophy before its unlocked(hidden flagged trophys will not allow you to see the description until you have unlcoked them)
+	char Unk02[3]; 			// Unkown data
+	char name[128]; 		// Trophy Name, EX: "UNLOCK ME"
+	char description[1024]; // Description, EX: "This is what needs to be unlocked"
 } OrbisNpTrophyDetails;
 
 // 
 typedef struct OrbisNpTrophyData 
 {
-	size_t size;
-	int32_t trophyId;
-	bool unlocked;
-	char Unk00[3];
-	OrbisRtcTick timestamp;
+	size_t size;			// this needs to be set with sizeof(OrbisNpTrophyData) or any function that takes this structure as a parameter will return a error(0x80551604)
+	int32_t trophyId;		// Trophy ID, 1-XXXX(0 should be the Platinum Trophy)
+	bool unlocked;			// flag that tells if the user has unlocked the Trophy)
+	char Unk00[3];			// likely padding, not sure why they didn't make unlocked a 32-bit value and swap a single bit for it, it would make it automaticlly alligned.
+	OrbisRtcTick timestamp;	// RTC time stamp of when the trophy is unlocked(this isn't fully accurate due to the RTC drifting, it can be off anywhere from +1 second to a whole minute+ behind.)
 } OrbisNpTrophyData;
 
 typedef struct OrbisNpTrophyScreenshotTarget 
 {
-	int32_t context;
-	int32_t trophyId;
+	int32_t context;	// same context that comes form sceNpTrophyCreateContext once created.
+	int32_t trophyId;	// Trophy ID, 1-XXXX(0 should be the Platinum Trophy)
 } OrbisNpTrophyScreenshotTarget;
 
 /* Np Online*/
 // 
 typedef struct OrbisNpOnlineId 
 {
-	char data[16];
-	char term;
-	uint8_t UNK[3];
+	char data[16]; 	// account name, follows PSN schema, min 3/max 16
+	char term; 		// terminator, always '\0'?
+	uint8_t UNK[3]; // Unkown, probably reserved/padding?
 } OrbisNpOnlineId;
 
 // 
