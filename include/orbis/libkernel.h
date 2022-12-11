e289142bfccb5dd0bb0433fc20c5288a127634bf
Greg <Greg@hackingadventures.ca>
2022-12-10 18:19:55 -0700

    Add appinfo data type and functions.

--- include/orbis/libkernel.h
+++ include/orbis/libkernel.h
@@ -148,7 +148,7 @@ int sceKernelFutimes(int, const OrbisKernelTimeval *);
 // Empty Comment
 void sceKernelGetAllowedSdkVersionOnSystem();
 // Empty Comment
-void sceKernelGetAppInfo();
+int sceKernelGetAppInfo(pid_t pid, OrbisAppInfo* info);
 // Empty Comment
 void sceKernelGetBackupRestoreMode();
 // Empty Comment
