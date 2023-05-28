2adac6fd7ba2e5771fba396b3f39765ad5c0a391
Leonid Pavel <pipehuffer@cocaine.ninja>
2023-05-28 23:33:46 +0000

    gnmdriver: complete definitions for some cmd and shader related functions

--- include/orbis/_types/gnm.h
+++ include/orbis/_types/gnm.h
@@ -2,11 +2,12 @@
 
 #include <stdint.h>
 
-typedef union OrbisGnmDrawIndexFlags {
+typedef union {
 	struct {
 		uint32_t predication : 1;
 		uint32_t _unused : 28;
 		uint32_t rendertargetsliceoffset : 3;
 	};
 	uint32_t asuint;
-} OrbisGnmDrawIndexFlags;
+} OrbisGnmDrawFlags;
+_Static_assert(sizeof(OrbisGnmDrawFlags) == 0x4, "");
