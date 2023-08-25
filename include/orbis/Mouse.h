cee9e9aaf00047ca95798f742dafb7b1427e9605
Backporter <29647778+Backporter@users.noreply.github.com>
2023-08-25 13:12:15 -0400

    Update Mouse.h

--- include/orbis/Mouse.h
+++ include/orbis/Mouse.h
@@ -2,25 +2,21 @@
 #define _SCE_MOUSE_H_
 
 #include <stdint.h>
+#include "orbis/_types/mouse.h"
 
 #ifdef __cplusplus 
-extern "C" {
+extern "C" 
+{
 #endif
 
+int32_t sceMouseInit();
+int32_t sceMouseOpen(int32_t userID, int32_t type, int32_t index, const OrbisMouseOpenParam*);
+int32_t sceMouseClose(int32_t);
+int32_t sceMouseRead(int32_t handle, OrbisMouseData*, int32_t num);
 
-// Empty Comment
-void sceMouseClose();
-// Empty Comment
-void sceMouseInit();
-// Empty Comment
-void sceMouseOpen();
-// Empty Comment
-void sceMouseRead();
-// Empty Comment
 void sceMouseSetPointerSpeed();
 
 #ifdef __cplusplus
 }
 #endif
-
 #endif
